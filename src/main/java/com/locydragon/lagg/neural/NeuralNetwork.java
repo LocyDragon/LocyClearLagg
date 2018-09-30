6b6e52136e79e10846effe2e5734e96c2179c411
LocyDragon <2424441676@qq.com>
2018-09-30 22:14:40 +0800

    采集机器学习的数据ing

--- src/main/java/com/locydragon/lagg/neural/NeuralNetwork.java
+++ src/main/java/com/locydragon/lagg/neural/NeuralNetwork.java
@@ -3,6 +3,7 @@ package com.locydragon.lagg.neural;
 import com.locydragon.lagg.listeners.ache.Ache;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.learning.LMS;
 import org.neuroph.util.TransferFunctionType;
 
 import java.io.File;
@@ -27,16 +28,21 @@ public class NeuralNetwork {
 			actor = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 10, 1);
 			{
 				dataTraining.addRow(new double[]{3586, 218.780}, new double[]{1});
+				dataTraining.addRow(new double[]{3587, 208.780}, new double[]{1});
+				dataTraining.addRow(new double[]{3400, 331.780}, new double[]{1});
+				dataTraining.addRow(new double[]{3587, 208.780}, new double[]{1});
 			}
 			{
 				dataTraining.addRow(new double[]{3803, 6.6}, new double[]{0});
 				dataTraining.addRow(new double[]{3839, 2.0}, new double[]{0});
 				dataTraining.addRow(new double[]{3812, 218.0}, new double[]{0});
 				dataTraining.addRow(new double[]{3807, 70.0}, new double[]{0});
-				dataTraining.addRow(new double[]{3721, 56.0}, new double[]{0.4});
+				dataTraining.addRow(new double[]{3721, 56.0}, new double[]{0.2});
 				dataTraining.addRow(new double[]{0, 10000.0}, new double[]{0});
 				dataTraining.addRow(new double[]{255, 28674.0}, new double[]{0});
 			}
+			((LMS)actor.getLearningRule()).setMaxError(0.001);
+			actor.learn(dataTraining);
 			actor.save(dataFileURL);
 		}
 		actor = MultiLayerPerceptron.createFromFile(dataFileURL);
